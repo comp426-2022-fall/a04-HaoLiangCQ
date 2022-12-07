89617a61a380378548f227a709bb312e20e133b5
Hao Liang <hliangcq@gmail.com>
2022-12-07 17:52:01 -0500

    a04_new

--- server.js
+++ server.js
@@ -1,31 +1,33 @@
 #!usr/bin/env node
-import { roll } from "./lib/roll.js"
-import minimist from "minimist"
-import express from 'express'
+import { roll } from "./lib/roll.js";
+import minimist from "minimist";
+import express from "express";
 
-const args = minimist(process.argv.slice(2))
-const port = args.port || 5000
-const app = express()
+
+const args = minimist(process.argv.slice(2));
+const port = args.port || 5000;
+const app = express();
 
 app.use(express.urlencoded({extended: true}));
 
 app.get('/app/', (req, res) => {
-	res.status(200).send("200 OK")
+    // return 200 ok
+	res.status(200).send("200 OK");
 });
 app.get('/app/roll', (req, res) => {
-    res.json(roll(6, 2, 1))
+    res.json(roll(6, 2, 1));
 });
 
 app.post('/app/roll/', (req, res) => {
-    let sides = parseInt(req.body.sides)
-    let dice = parseInt(req.body.dice)
-    let rolls = parseInt(req.body.rolls)
-    res.status(200).json(roll(sides,dice,rolls))
+    let sides = parseInt(req.body.sides);
+    let dice = parseInt(req.body.dice);
+    let rolls = parseInt(req.body.rolls);
+    res.status(200).json(roll(sides,dice,rolls));
 });
 
 app.get('/app/roll/:sides/', (req, res) => {
-    let sides = parseInt(req.params.sides)
-    res.status(200).json(roll(sides, 2, 1))
+    let sides = parseInt(req.params.sides);
+    res.status(200).json(roll(sides, 2, 1));
 })
 
 app.get('/app/roll/:sides/:dice/', (req, res) => {
